7216d366c456f95671a8607081da0ef45fef09e6
dbsalazarr <dbsalazarr@gmail.com>
2024-10-05 20:52:45 -0500

    Creación de la bd desde Django

--- humedad_server/humedad/views.py
+++ humedad_server/humedad/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import humedad
+from django.http import HttpResponse
+from .models import Humedad
 
 # Create your views here.
 def listar_humedad(request) :
-    humedads = humedad.object.all()
-    return HttpResponse(", ".join([humedad.humedad_analog for hume in humedads]))
+    humedads = Humedad.objects.all()
+    return HttpResponse(", ".join([hume.humedad_analog for hume in humedads]))
